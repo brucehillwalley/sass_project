a7ff1850f9fb066d7a91dd2e08e575335d3eaa7b
BruceHill <brucehillwalley@gmail.com>
2023-11-09 23:37:51 +0300

    3.saat sonu

--- project-inclass/js/app.js
+++ project-inclass/js/app.js
@@ -1,1 +1,9 @@
 //? JS
+const hamburgerBtn=document.querySelector(".fa-bars")
+const navbarMenu=document.querySelector("#navbarMenu");
+hamburgerBtn.addEventListener("click",()=>{
+
+    const headerLinks=document.querySelector(".header__links")
+headerLinks.classList.toggle("show")
+
+})
